67652cef76ae0bcc62373f9f285045df76b45430
Henrique Weiand <henriqueweiand@gmail.com>
2020-05-04 18:59:59 -0300

    update

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,45 +1,25 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import * as dotenv from 'dotenv';
 
-let ormconfig;
-
-// teste
 if (process.env.NODE_ENV !== 'production') {
   dotenv.config();
-
-  ormconfig = {
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'password',
-    database: 'scrumpoker',
-    synchronize: true,
-    migrationsRun: false,
-    keepConnectionAlive: false,
-    entities: [`${__dirname}/**/**/*.entity{.ts,.js}`],
-    migrations: [`${__dirname}/migrations/*{.ts,.js}`],
-    cli: {
-      migrationsDir: 'src/migrations',
-    },
-  } as TypeOrmModuleOptions;
-} else {
-  ormconfig = {
-    type: 'postgres',
-    host: process.env.TYPEORM_HOST,
-    port: Number(process.env.TYPEORM_PORT),
-    username: process.env.TYPEORM_USERNAME,
-    password: process.env.TYPEORM_PASSWORD,
-    database: process.env.TYPEORM_DATABASE,
-    synchronize: Boolean(process.env.TYPEORM_SYNCHRONIZE || false),
-    migrationsRun: Boolean(process.env.TYPEORM_MIGRATIONS_RUN || false),
-    keepConnectionAlive: false,
-    entities: [`${__dirname}/**/**/*.entity{.ts,.js}`],
-    migrations: [`${__dirname}/migrations/*{.ts,.js}`],
-    cli: {
-      migrationsDir: 'src/migrations',
-    },
-  } as TypeOrmModuleOptions;
 }
 
-export = ormconfig;
+const config: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: process.env.TYPEORM_HOST || 'localhost',
+  port: Number(process.env.TYPEORM_PORT || 5432),
+  username: process.env.TYPEORM_USERNAME || 'postgres',
+  password: process.env.TYPEORM_PASSWORD || 'password',
+  database: process.env.TYPEORM_DATABASE || 'scrumpoker',
+  synchronize: Boolean(process.env.TYPEORM_SYNCHRONIZE || false),
+  migrationsRun: Boolean(process.env.TYPEORM_MIGRATIONS_RUN || false),
+  entities: [`${__dirname}/**/**/*.entity{.ts,.js}`],
+  migrations: [`${__dirname}/migrations/*{.ts,.js}`],
+  keepConnectionAlive: false,
+  cli: {
+    migrationsDir: 'src/migrations',
+  },
+};
+
+export = config;
